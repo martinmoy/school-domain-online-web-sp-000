69ff1db86f0141246d31ff5b4aacecd51dd03e5f
Martin Moy <martinmoy1991@gmail.com>
2020-07-19 05:40:14 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,6 +11,16 @@ class School
    @roster[grade] << name
  end
 
+ def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |key, value|
+         value.sort!
+    end
+  end
+
 
 
 end
